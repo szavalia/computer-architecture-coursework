7ee4ec51d69c224a85d6234eacde98e6fe655bf7
szvalia <szavalia@itba.edu.ar>
2020-06-07 19:32:18 -0300

    De verdad no entendía nada: ahora compila

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,13 +1,20 @@
 
 #define ZERO_EXCEPTION_ID 0
-
+#define INVALID_OPCODE_EXCEPTION_ID 6
 static void zero_division();
+static void invalid_opcode();
 
 void exceptionDispatcher(int exception) {
 	if (exception == ZERO_EXCEPTION_ID)
 		zero_division();
+	else if(exception ==  INVALID_OPCODE_EXCEPTION_ID)
+		invalid_opcode();
 }
 
 static void zero_division() {
 	// Handler para manejar excepcíon
+}
+
+static void invalid_opcode(){
+	// Handler para manejar la excepción
 }
