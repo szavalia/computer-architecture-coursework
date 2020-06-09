bc6e30dbdc0298bef1976ffb1c02fbec5e188ecf
vriera <57446063+vriera@users.noreply.github.com>
2020-06-09 20:43:55 -0300

    Limpieza Data
    cleaning

--- Userland/SampleDataModule/include/calculadora.h
+++
@@ -1,5 +0,0 @@
-#ifndef _CALCULADORA_H_
-#define _CALCULADORA_H_
-    int calculate(char * string , double * answer);
-    void launch_calculator();
-#endif
--- Userland/SampleDataModule/sampleDataModule.c
+++ Userland/SampleDataModule/sampleDataModule.c
@@ -1,12 +1,6 @@
-/* sampleCodeModule.c */
-#include "usr_lib.h"
-#include "calculadora.h"
-
-
 
 int main() 
 {
-	launch_calculator();
 	return 0x00000CACAC0DE;
 }
 
